5a5996e8625610942cb9aecb9769304642487ac7
Parthasarathy B <pbakthavatsalam@ctrl.nl>
2020-05-30 13:30:52 +0200

    example

--- PIR_motion_sensor/index.js
+++ PIR_motion_sensor/index.js
@@ -1,1 +1,23 @@
-console.log('works')
+var gpio = require("gpio");
+var gpio22, gpio4, intervalTimer;
+ 
+// Flashing lights if LED connected to GPIO22
+gpio22 = gpio.export(22, {
+   ready: function() {
+      intervalTimer = setInterval(function() {
+         gpio22.set();
+         setTimeout(function() { gpio22.reset(); }, 500);
+      }, 1000);
+   }
+});
+
+
+// reset the headers and unexport after 10 seconds
+setTimeout(function() {
+  clearInterval(intervalTimer);          // stops the voltage cycling
+  gpio22.removeAllListeners('change');   // unbinds change event
+  gpio22.reset();                        // sets header to low
+  gpio22.unexport(() => {
+    process.exit();
+  });                     // unexport the header
+}, 10000)

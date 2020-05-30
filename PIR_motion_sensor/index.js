8fabbdfe119d4ee569d8464789cca8742966330b
Parthasarathy B <pbakthavatsalam@ctrl.nl>
2020-05-30 14:53:51 +0200

    IIFE

--- PIR_motion_sensor/index.js
+++ PIR_motion_sensor/index.js
@@ -1,41 +1,46 @@
-const Gpio = require('onoff').Gpio; // Gpio class
-const LCD = require('raspberrypi-liquid-crystal');
+(
+  async () => {
+    const Gpio = require('onoff').Gpio; // Gpio class
+    const LCD = require('raspberrypi-liquid-crystal');
 
-const lcd = new LCD(1, 0x27, 16, 2);
-lcd.beginSync();
-await lcd.printLine(0, 'Status')
-await lcd.printLine(1, 0)
-const led = new Gpio(17, 'out');       // Export GPIO17 as an output
-const msensor = new Gpio(22, 'in', 'both');
-let stopSystem = false;
- 
-// Toggle the state of the LED connected to GPIO17 every 200ms
-const blinkLed = _ => {
-  if (stopSystem) {
-    led.unexport();
-    msensor.unexport();
-    lcd.noDisplay()
-    return;
+    const lcd = new LCD(1, 0x27, 16, 2);
+    lcd.beginSync();
+    await lcd.printLine(0, 'Initialized')
+    const led = new Gpio(17, 'out');       // Export GPIO17 as an output
+    const msensor = new Gpio(22, 'in', 'both');
+    let stopSystem = false;
+    
+    // Toggle the state of the LED connected to GPIO17 every 200ms
+    const blinkLed = _ => {
+      if (stopSystem) {
+        led.unexport();
+        msensor.unexport();
+        lcd.noDisplay()
+        return;
+      }
+    
+      led.read()
+        .then(value => led.write(value ^ 1))
+        .then(_ => setTimeout(blinkLed, 200))
+        .catch(err => console.log(err));
+    };
+
+    msensor.watch(async (err, value) => {
+      console.log(value)
+      await lcd.printLine(0, 'Status')
+      await lcd.printLine(1, value)
+    })
+    process.on('SIGINT', _ => {
+      //console.log('releasing resources')
+      //msensor.unexport();
+      //console.log('resources released')
+    });
+
+    blinkLed();
+    
+    // Stop blinking the LED after 5 seconds
+    setTimeout(_ => stopSystem = true, 15000);
+
   }
- 
-  led.read()
-    .then(value => led.write(value ^ 1))
-    .then(_ => setTimeout(blinkLed, 200))
-    .catch(err => console.log(err));
-};
 
-msensor.watch(async (err, value) => {
-  console.log(value)
-  await lcd.printLine(0, 'Status')
-  await lcd.printLine(1, value)
-})
-process.on('SIGINT', _ => {
-  //console.log('releasing resources')
-  //msensor.unexport();
-  //console.log('resources released')
-});
-
-blinkLed();
- 
-// Stop blinking the LED after 5 seconds
-setTimeout(_ => stopSystem = true, 15000);
+)()
